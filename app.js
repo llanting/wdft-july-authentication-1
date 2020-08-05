ca80fd99bfeecf5302e1f8c39778de4c25ed53b9
Lanette Lanting <lanettelanting@gmail.com>
2020-08-05 11:29:40 +0200

    A change

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(session({
   },
   store: new MongoStore({
     mongooseConnection: mongoose.connection,
-    ttl: 60*60*24 // 1 day // value in seconds
+    ttl: 60*60*8 // 1 day // value in seconds
   })
 }));
 
